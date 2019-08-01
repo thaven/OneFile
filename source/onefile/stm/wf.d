48ab78cb05b6c3cd23e885ed6eedb7d7178ce446
Harry T. Vennik <htvennik@gmail.com>
2019-08-01 15:38:21 +0200

    Move test if type is @nogc destroyable into separate template

--- source/onefile/stm/wf.d
+++ source/onefile/stm/wf.d
@@ -1774,6 +1774,15 @@ if (is(T == class))
     return (cast(void*)obj)[0 .. obj.classinfo.init.length];
 }
 
+template isNoGcDestroyable(T)
+if (is(T == class))
+{
+    static if (__traits(hasMember, T, "__dtor"))
+        enum bool isNoGcDestroyable = hasFunctionAttributes!(T.__dtor, "@nogc");
+    else
+        enum bool isNoGcDestroyable = true;
+}
+
 @nogc
 void disposeNoGc(A, T)(auto ref A alloc, auto ref T obj)
 if (is(T == class))
@@ -1783,9 +1792,8 @@ if (is(T == class))
     static assert(hasFunctionAttributes!(A.deallocate, "@nogc"),
             "Cannot use disposeNoGc with GC reliant allocator");
 
-    static if (__traits(hasMember, T, "__dtor"))
-        static assert(hasFunctionAttributes!(T.__dtor, "@nogc"),
-                "Destructor of " ~ fullyQualifiedName!T ~ "is not @nogc");
+    static assert(isNoGcDestroyable!T,
+            "Destructor of " ~ fullyQualifiedName!T ~ "is not @nogc");
 
     (cast(void function(ref A, ref T) @nogc) (ref A alloc, ref T obj) {
         alloc.dispose(obj);
